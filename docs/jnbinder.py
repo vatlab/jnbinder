7bd377aad57297be90ed3d8e2f9590be7c16e038
Gao Wang <gaow@uchicago.edu>
2018-02-22 15:12:29 -0600

    Remove google analytics

--- docs/jnbinder.py
+++ docs/jnbinder.py
@@ -186,14 +186,6 @@ def get_index_tpl(conf, dirs):
 {%%- extends 'basic.tpl' -%%}
 
 {%%- block header -%%}
-<!-- Global Site Tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-107286198-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)};
-  gtag('js', new Date());
-  gtag('config', 'UA-107286198-1');
-</script>
 {{ super() }}
 
 <!DOCTYPE html>
