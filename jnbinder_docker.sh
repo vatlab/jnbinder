a49d48e18234c6970e7054f30e1f0bd69a4ab338
Gao Wang <wangow@gmail.com>
2021-02-03 10:30:00 -0500

    add docker files

--- jnbinder_docker.sh
+++ jnbinder_docker.sh
@@ -1,1 +1,1 @@
-export alias="docker run --rm  -v $PWD:$PWD -v $PWD/docs:$PWD/docs -v /tmp:/tmp   -t -w=$PWD -u $UID:1000 gaow/jnbinder"
+export alias jnbinder="docker run --rm  -v $PWD:$PWD -v $PWD/docs:$PWD/docs -v /tmp:/tmp   -t -w=$PWD -u $UID:1000 gaow/jnbinder"
